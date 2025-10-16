c92c20f4517eb394b2cdd8e41e999fad3c66ddba
Pedro Quinellato <pedroqdvieira88123@gmail.com>
2025-10-16 10:29:05 -0300

    chore: inicializa o repositório com a estrutura do projeto PooEstudo, incluindo arquivos de configuração, ponto de entrada e modelos de dados

---
+++ Models/Pessoa.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+
+namespace PooEstudo.Models
+{
+    public class Pessoa
+    {
+
+        private string _nome;
+        public string Nome
+        {
+            get => _nome.ToUpper();
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("O nome não pode ser vazio ou somente espaços em branco");
+                }
+                _nome = value;
+            }
+        }
+
+        public string Sobrenome { get; set; }
+
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+
+        private int _idade;
+        public int Idade
+        {
+            get => _idade;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser negativa");
+                }
+                _idade = value;
+            }
+        }
+
+        public void Apresentar()
+        {
+            System.Console.WriteLine($"Nome: {NomeCompleto}, Idade: {Idade}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PooEstudo.Models;
+
+Pessoa p1 = new Pessoa();
+
+System.Console.WriteLine("Informe o seu nome: ");
+p1.Nome = Console.ReadLine();
+
+System.Console.WriteLine("Informe o seu sobrenome: ");
+p1.Sobrenome = Console.ReadLine();
+
+System.Console.WriteLine("Informe a sua idade: ");
+p1.Idade = Convert.ToInt32(Console.ReadLine());
+p1.Apresentar();
